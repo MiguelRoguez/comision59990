3252865656074e01c579f9043c0d9b2b136cf9da
miguel rodriguez <rodriguez.planeacion@gmail.com>
2024-10-02 15:57:38 -0600

    Preentrega_1

---
+++ MercadoLiebre/js/index.js
@@ -0,0 +1,21 @@
+let montoTotal = 20000;
+let numeroCuotas = 10;
+let tasaInteres = 8;
+
+let cuotaMensual = calcularCuotas(montoTotal, numeroCuotas, tasaInteres);
+
+function calcularCuotas(montoTotal, numeroCuotas, tasaInteres) {
+  // Convertir la tasa de interés a decimal
+  let tasaInteresDecimal = tasaInteres / 100;
+
+  // Calcular el valor de cada cuota utilizando la fórmula de amortización francesa
+  let cuota =
+    (montoTotal *
+      tasaInteresDecimal *
+      Math.pow(1 + tasaInteresDecimal, numeroCuotas)) /
+    (Math.pow(1 + tasaInteresDecimal, numeroCuotas) - 1);
+
+  return cuota;
+}
+
+console.log("La cuota mensual es:", cuotaMensual.toFixed(2));
